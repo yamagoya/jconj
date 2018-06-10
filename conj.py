fb7a23348f3df4f3848ecb53c239695ffa35cf22
Stuart McGraw <stuart@soga.home>
2018-06-10 16:06:21 -0600

    Tweaks for running as independent project.
    - Extract doc from conj.py into independent README.txt file.
    - Add a README.txt file to the data/ directory.
    - In conj.py move the two conjugation functions to near the top.
    - Minor changes and improvements to command line argument processing.

--- conj.py
+++ conj.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #######################################################################
-#  This file is part of JMdictDB.
-#  Copyright (c) 2014 Stuart McGraw
+#  Copyright (c) 2014,2018 Stuart McGraw
 #
 #  JMdictDB is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published
@@ -19,121 +18,10 @@
 #  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 #######################################################################
 
-# This is an example program demonstrating how the JMdictDB word
-# conjugation tables can be used, idependently of the rest of the
-# JMdictDB API, to conjugate Japanese words.
-#
-# It will print a table of conjugations for the word given
-# on the command line.  You are required to supply the word's
-# part-of-speech code as used in wwwjdic, JMdict, et.al, since
-# this program in independent of JMdictDB and has no other way
-# to determine it.  Run this program with "--help" for full
-# details on the command line arguments and options.
-#
-# It would be a straight-forward exercise to change this code
-# to look the word up in a JMdict database to get the part-
-# of-speech value automatically or to format the output more
-# nicely, etc.
-#
-#-------------------------------------------------------------------
-# JMdictDB uses a set of four csv (comma separated value) tables
-# that contain information needed to conjugate a japanese word
-# based on the word's part-of-speech code (e.g., v1 verb, v5k verb,
-# i-adjective, etc).  In JMdictDB these data are loaded into database
-# tables and the word conjugation done via SQL views.
-#
-# However it is also possible to using read these csv files directly
-# in an application program and do the conjugation in code; this
-# program is an example of that.
-#
-# This program conjugates words using primarily the conjugation
-# data in file .../conjo.csv.  That file consists of rows of 10
-# values per row:
-#
-#    pos:   A part-of-speech number.  These are defined in kwpos.csv
-#           and each number corresponds to a keyword like 'v1', 'v5k'
-#           'n', 'vs, adj-i', etc, as used in wwwjdict, JMdict.xml,
-#           etc.
-#    conj:  A conjugation number.  These are defined in conj.csv.
-#    neg:   If false, this row is for the affirmative conjugation
-#           form.  If true, for the negative form.
-#    fml:   If false, this row is for the plain conjugation form.
-#           If true, for the formal (aka polite) form.
-#    onum:  A disambiguating number (starting from 1) for rows
-#           containing variant okurigana for the same conjugation
-#           (e.g., ～なくて and ～ないで).
-#
-#    stem:  The number of characters to remove from the end of a
-#           word before adding okurigana.
-#    okuri: The okurigana text for the conjugated form.
-#    euphr: Replacement kana text for the verb stem in cases where
-#           there is a euphonic change in the stem (e.g. く -> こ in
-#           来る -> 来ない).
-#    euphk: Replacement kanji text when there is a kanji change in
-#           the stem.  (The only case of this is for the Potential
-#           conjugation form of suru: 為る・する -> 出来る・できる).
-#    pos2:  Not currently used.
-#
-# The first five items form a unique key identifying each
-# conjugation and are used as a key in the python dicts in the
-# code below to identify conjugations.  The remaining items in
-# each row are used to construct the conjugated form of a word
-# for the conjugation specified by the key.
-#
-# The algorithm for conjugating a word is: 
-# 1. For each (or a desired) conjugation (identified by the
-#    tuple (pos,conj,neg,fml,onum)), get stem, okuri, euphr
-#    and euphk from conjo.csv.
-# 2. If euphr or euphk is non-null, determine if the word to
-#    be conjugated is kanji or kana.  In the general case there
-#    is no easy way of doing this since it is hard to tell how
-#    long the word is if it occurs at the end of a longer phrase.
-#    But since the only PoS values that have euphr or euphk values
-#    currently are for the words: いい,　来る・くる and 為る・する we
-#    can look at the next-to-last character to see if it is kanji
-#    or kana.
-# 3. Conjugate the word:
-# 3a. If euphr and euphk are null, remove 'stem' characters from
-#    the end of the text.
-# 3b. Else if 'euphk' is not null and the word is kanji, remove
-#    stem+1 characters from the end of the text and append 'euphk'
-#    to the end of the text. 
-# 3c. Else if 'euphr' is not null and the word is kana, remove
-#    stem+1 characters from the end of the text and append 'euphr'
-#    to the end of the text. 
-# 3d. Append 'okuri' to the end of the text.
-#
-# (See function construct() below.)
-#
-# The other csv tables are:
-#    conj.csv: Maps each conj number to a descriptive text.
-#    conotes.csv: A set of test notes, each identified by an
-#         id number.
-#    conjo_notes.csv: Maps notes in conotes.csv (identified
-#         by id number) to conjugations in conjo.csv
-#         (identified by (pos,conj,neg,fml,onum)).
-#
-# This program reads the csv files because JMdictDB requires the
-# conjugation data in that form to load into database tables.  But
-# it should be clear that the conjugation data could alternatively
-# be included in a program in the form of constant, embedded
-# tables, possibly in a more conveniently structured form such as
-# is currently returned by read_conj_tables(), rather than reading
-# the csv files each time.  One could also eliminate the pos numbers,
-# substituting the corresponding abbreviation string wherever the
-# numbers occur.
-#
-# See also:
-# pg/mkviews.sql -- contains SQL views that perform the same
-# operations as this program using the .csv data read into
-# database tables.  Those views (primarily "vinfl" around
-# line 670) provide the conjugated words for the web/cgi/conj.py
-# JMdictDB web page.
-
 import sys, os, csv, re, collections, pdb
 
 def main ():
-        args = parse_args() # Parse command line args.
+        args = parse_args() # Parse command line, use --help for info.
           # read the conjugation .csv files into a single data structure.
           # See read_conj_tables() for description of 'ct's structure.
         ct = read_conj_tables (args.dir)
@@ -142,10 +30,12 @@ def main ():
           # Convert the given pos keyword into pos id number.
         try: pos = ct['kwpos'][args.pos][0]
         except KeyError:
-            print ("part-of-speech '%s' is not conjugatable"
-                   % args.pos, file=sys.stderr)
-            return 1
-          # Generate conjugations for the word assuming 'pos' as part-of-speech.
+            sys.exit ("unknown part-of-speech: %s\n'conj.py --list' will "
+                "print a list of conjugatable parts-of-speech" % args.pos)
+        if pos not in [x[0] for x in ct['conjo']]:
+            sys.exit ("no conjugation data available for part-of-speech: %s\n"
+                "'conj.py --list' will "
+                "print a list of conjugatable parts-of-speech" % args.pos)
         conjs = conjugate (args.kanj, args.kana, pos, ct)
           # Some conjugations have multiple forms (e.g. ~なくて and ~ないで) that
           # are disinguished by 'onum' in the conjugation key.  The following
@@ -158,6 +48,78 @@ def main ():
         for n in sorted (notes):
             print ("[%s] -- %s" % (n, ct['conotes'][n][1]))
 
+###############################################################################
+# The following two functions constitute the conjugator, everything else
+# provides support only (formatting, printing, reading the data tables, etc.)
+
+def conjugate (ktxt, rtxt, pos, ct):
+        '''\
+        Generate a dict containing all the conjugated forms of the kanji
+        and/or kana texts 'ktxt' and 'rtxt'.
+
+        Parameters:
+          ktxt -- (str) Kanji text of the word to be conjugated.
+          rtxt -- (str) Reading text of the word to be conjugated.
+          pos -- (int) Id number for the part-of-speech of the word
+              to be conjugated.
+          ct -- Conjugation table.  This data for these are in the "data/"
+              subdirectory and may be read in with read_conj_tables().
+
+        Returns:
+          A dictionary whose keys are 5-tuples:
+            pos: Part-of-speech number (all the generated conjugations
+                   will have the same pos value which will be the same
+                   as parameter 'pos'.)
+            conj: The conjugation number (an id field value from conj.id)
+            neg: A bool, false for affirmative conjugation, true for negative.
+            fml: A bool, false for plain, true for formal (-masu) form.
+            onum: Int index (starting from one) to disambiguate conjugations
+                   that have multiple forms (e.g, ～なくて and ～ないで).
+          These keys are of the same form as used in the 'ct' conjugation
+          table, see read_csv_files() for more details.
+          The value of each item is a string with the combined conjugated
+          form of 'ktxt' and 'rtxt' for that conjugation.
+        '''
+
+        conjs = {}
+          # Get pos number from kw:
+        for conj,conjnm in sorted (ct['conj'].values(), key=lambda x:x[0]):
+            for neg, fml in (0,0),(0,1),(1,0),(1,1):
+                neg, fml = bool (neg), bool (fml)
+                for onum in range(1,10):  # onum values start at 1, not 0.
+                    try: _,_,_,_,_, stem, okuri, euphr, euphk, _ \
+                      = ct['conjo'][pos,conj,neg,fml,onum]
+                    except KeyError: break;
+                    kt = construct (ktxt, stem, okuri, euphr, euphk) \
+                         if ktxt else ''
+                    rt = construct (rtxt, stem, okuri, euphr, euphk) \
+                         if rtxt else ''
+                    txt = (kt + '【' + rt + '】') if kt and rt else (kt or rt)
+                    conjs[(pos,conj,neg,fml,onum)] = txt
+        return conjs
+
+def construct (txt, stem, okuri, euphr, euphk):
+        '''Given a word (in kanji or kana), generate its conjugated form by
+        by removing removing 'stem' characters from its end (and an additional
+        character if the word is kana and 'euphr' is true or the word is in
+        kanji and 'euphk' are true), then appending either 'euphr' or 'euphk'.
+        We determine if the word is kanji or kana by looking at its next-to-
+        last character.  Finally, 'okuri' is appended.'''
+
+        if len (txt) < 2: 
+            raise ValueError ("Conjugatable words must be at least"
+                              " 2 characters long")
+        iskana = txt[-2] > 'あ' and txt[-2] <= 'ん'
+        if iskana and euphr or not iskana and euphk: stem += 1
+        if iskana: conjtxt = txt[:-stem] + (euphr or '') + okuri
+        else:      conjtxt = txt[:-stem] + (euphk or '') + okuri
+        return conjtxt
+
+###############################################################################
+# The remainder of this module is support functions for things like
+# formatting and printing the conjugation results, reading the conjugation
+# data tables, parsing the command line arguments, etc.
+ 
 def print_conjs (conjs, ct):
         'Print the conjugation table returned by combine_onums().'
           # Create a dictionary to map combinations of 'neg' and 'fml' in the
@@ -168,7 +130,8 @@ def print_conjs (conjs, ct):
           # of the given kanji and kana) and print them.
         for key,txt in sorted (conjs.items()):
             pos,conj,neg,fml = key
-              # Get the conjugation description from the conjugation number 'conj'.
+              # Get the conjugation description from the conjugation
+              #  number 'conj'.
             conjdescr = ct['conj'][conj][1]
             print ("%-20s %s %s" % (conjdescr, labels[(neg,fml)], txt))
 
@@ -215,50 +178,6 @@ def print_help (ct_):
         #print ("Available conjugations:")
         #for conj, descrip in sorted (ct['conj'].values(), key=lambda x:x[0]):
         #    print ("%s\t%s" % (conj, descrip))
-
-def conjugate (ktxt, rtxt, pos, ct):
-        '''Generate a dict containing all the conjugated forms of the kanji
-        and/or kana texts 'ktxt' and 'rtxt'.  Each conjugation is described
-        by the same key used in 'ct' (see read_csv_files()):
-             pos: Part-of-speech number (all the generated conjugations
-                    will have the same pos value which will be the same
-                    as parameter 'pos'.)
-             conj: The conjugation number (an id field value from conj.id)
-             neg: A bool, false for affirmative conjugation, true for negative.
-             fml: A bool, false for plain, true for formal (-masu) form.
-             onum: Int index (starting from one) to disambiguate conjugations
-                    that have multiple forms (e.g, ～なくて and ～ないで).
-        The value of each item is a string with the combined conjugated form
-        of 'ktxt' and 'rtxt' for that conjugation.
-        '''
-        conjs = {}
-          # Get pos number from kw:
-        for conj,conjnm in sorted (ct['conj'].values(), key=lambda x:x[0]):
-            for neg, fml in (0,0),(0,1),(1,0),(1,1):
-                neg, fml = bool (neg), bool (fml)
-                for onum in range(1,10):  # onum values start at 1, not 0.
-                    try: _,_,_,_,_, stem, okuri, euphr, euphk, _ = ct['conjo'][pos,conj,neg,fml,onum]
-                    except KeyError: break;
-                    kt = construct (ktxt, stem, okuri, euphr, euphk) if ktxt else ''
-                    rt = construct (rtxt, stem, okuri, euphr, euphk) if rtxt else ''
-                    txt = (kt + '【' + rt + '】') if kt and rt else (kt or rt)
-                    conjs[(pos,conj,neg,fml,onum)] = txt
-        return conjs
-
-def construct (txt, stem, okuri, euphr, euphk):
-        '''Given a word (in kanji or kana), generate its conjugated form by
-        by removing removing 'stem' characters from its end (and an additional
-        character if the word is kana and 'euphr' is true or the word in in kanji
-        and 'euphk' are true), then appending either 'euphr' or 'euphk'.  We 
-        determine if the word is kanji or kana by looking at its next-to-last
-        character.  Finally, 'okuri' is appended.'''
-
-        if len (txt) < 2: raise ValueError ("Conjugatable words must be at least 2 characters long")
-        iskana = txt[-2] > 'あ' and txt[-2] <= 'ん'
-        if iskana and euphr or not iskana and euphk: stem += 1
-        if iskana: conjtxt = txt[:-stem] + (euphr or '') + okuri
-        else:      conjtxt = txt[:-stem] + (euphk or '') + okuri
-        return conjtxt
 
 # The following functions read the .csv conjugation data.
 
@@ -323,10 +242,11 @@ def read_conj_tables (dir):
                   # data needed for a specific conjugation.
                 ct[fn] = dict (((tuple(row[0:5]),row) for row in csvtbl))
             elif fn == 'conjo_notes':
-                  # conjo_notes maps multiple conjugations (pos,conj,neg,fml,onum)
-                  # to multiple note numbers.  So instead of using a dictionary
-                  # keyed by conjugation and where each value is a row, we use one
-                  # where each value is a list of note numbers for that conjugation.
+                  # conjo_notes maps multiple conjugations (pos,conj,neg,fml,
+                  #  onum) to multiple note numbers.  So instead of using a
+                  #  dictionary keyed by conjugation and where each value is
+                  #  a row, we use one where each value is a list of note
+                  # numbers for that conjugation.
                 ct[fn] = d = collections.defaultdict (list)
                 for row in csvtbl: d[tuple(row[0:5])].append (row[5])
             else:
@@ -334,10 +254,11 @@ def read_conj_tables (dir):
                   # of the first column which is an id number.
                 ct[fn] = dict (((row[0],row) for row in csvtbl))
                   # Do the same to kwpos.csv but in addition add the same row
-                  # with a key of the 2nd column (the kw abbr string.)  This will
-                  # allow us to look up kwpos records by either id number or
-                  # keyword string.
-                if fn == 'kwpos': ct[fn].update (((row[1],row) for row in csvtbl))
+                  # with a key of the 2nd column (the kw abbr string.)  This 
+                  # will allow us to look up kwpos records by either id number
+                  # or keyword string.
+                if fn == 'kwpos': ct[fn].update (((row[1],row) 
+                                                  for row in csvtbl))
         return ct
 
 def readcsv (filename, coltypes, hasheader):
@@ -379,7 +300,7 @@ def parse_word (args):
         ''''args' is a list of one or two strings that are the kanji, kana
         arguments from the command line.  If two, we take them to be in the
         order kanji, kana.  But if one, it could be either kanji or kana
-        and we indentify which by looking for any kanji character (>=0x4000)
+        and we identify which by looking for any kanji character (>=0x4000)
         in it.  We return separate kanji and kana strings accordingly.'''
 
         if len (args) == 1:
@@ -392,10 +313,11 @@ def parse_word (args):
 
 def parse_args (argv=None):
         p = ArgumentParser (add_help=False,
-            description="%prog will print a list of the conjugated forms of the "
-                "Japanese word given by the kanji and/or kana words given in the "
-                "ARGS argument(s).  POS is a part-of-speech code as used in "
-                "wwwjdic, JMdict, etc ('v1', 'v5k', 'adj-i', etc.)")
+            description="%prog will print a list of the conjugated forms "
+                "of the Japanese word given by the kanji and/or kana words "
+                "given in the ARGS argument(s).  POS is a part-of-speech "
+                "code as used in wwwjdic, JMdict, etc ('v1', 'v5k', "
+                "'adj-i', etc.)")
         p.add_argument ("pos", nargs='?',
             help="Part-of-speech code word as used in wwjdic, JMdict, etc.  "
                 "Run program with \"--list\" to get list of valid pos values.")
@@ -406,7 +328,7 @@ def parse_args (argv=None):
                 "to determine which is which.")
         p.add_argument ("--list", action="store_true", default=False,
             help="Print list of valid pos values to stdout and exit.")
-        p.add_argument ("-d", "--dir", default='../pg/data/',
+        p.add_argument ("-d", "--dir", default='./data',
             help="Directory where the conjugation csv data files are kept.")
         p.add_argument ("--help",
             action="help", help="Print this help message.")
